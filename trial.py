9ed0600485438c251f8503dbe894764277296d08
ss9914 <104421609+ss9914@users.noreply.github.com>
2023-10-08 11:46:14 +0530

    Created trial.py

---
+++ trial.py
@@ -0,0 +1,10 @@
+# Python code to illustrate the working of strip()
+string = '@@@@Geeks for Geeks@@@@@'
+
+# Strip all '@' from beginning and ending
+print(string.strip('@'))
+
+string = 'www.Geeksforgeeks.org'
+
+# '.grow' removes 'www' and 'org' and '.'
+print(string.strip('.grow'))
